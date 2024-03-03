532faaae380bab0467fca785bfb13870b9ba05c4
vlk2022 <volodymyrkostiuk2021@gmail.com>
2024-03-04 00:02:57 +0200

    commit

--- src/components/FormForSearch/FormForSearch.js
+++ src/components/FormForSearch/FormForSearch.js
@@ -1,70 +1,3 @@
-// import React, {useEffect} from 'react';
-// // import {useDispatch} from "react-redux";
-// // import {useForm} from "react-hook-form";
-// // import {useNavigate, useSearchParams} from "react-router-dom";
-// //
-// // import classes from "./FormForSearch.module.css";
-// // import {getSearchProducts, getTotalSearch} from "../../store/slices/search.slice";
-// //
-// //
-// // const FormForSearch = () => {
-// //     const dispatch = useDispatch();
-// //     const {register, handleSubmit, setValue} = useForm();
-// //     const navigate = useNavigate();
-// //
-// //     const [query, setQuery] = useSearchParams();
-// //
-// //     const page = 1;
-// //
-// //     const submit = (data) => {
-// //         setQuery({q: data.search});
-// //         const q = query.get('q');
-// //
-// //             if (q) {
-// //                 dispatch(getSearchProducts({ word: q, page }));
-// //                 dispatch(getTotalSearch({ word: q, page }));
-// //             }
-// //
-// //         setValue('search', q);
-// //
-// //         navigate(`search/${q}`)
-// //     }
-// //
-// //     // useEffect(() => {
-// //     //     const q = query.get('q');
-// //     //
-// //     //     if (q) {
-// //     //         dispatch(getSearchProducts({ word: q, page }));
-// //     //         dispatch(getTotalSearch({ word: q, page }));
-// //     //     }
-// //     //
-// //     //     setValue('search', q);
-// //     // }, [query]);
-// //
-// //     // const submit = (data) => {
-// //     //     setQuery({q: data.search});
-// //     //
-// //     //     navigate(`search/${data.search}`)
-// //     // }
-// //
-// //
-// //     return (
-// //         <main className={`${classes.wrap} width`}>
-// //             <form onChange={handleSubmit(submit)}>
-// //                 <input className={`${classes.searchInput} width`}
-// //                        defaultValue={''}
-// //                        {...register('search')}
-// //                        type="text"
-// //                        placeholder={'Search...'}
-// //                 />
-// //             </form>
-// //         </main>
-// //     );
-// // };
-// //
-// // export {FormForSearch};
-
-
 import React, {useEffect} from 'react';
 import {useDispatch} from "react-redux";
 import {useForm} from "react-hook-form";
@@ -96,7 +29,7 @@ const FormForSearch = () => {
         setValue('search', q);
 
         if (q) {
-        navigate(`/search/${q}`);
+            navigate(`/search/${q}`);
         } else {
             navigate(`/`);
         }
