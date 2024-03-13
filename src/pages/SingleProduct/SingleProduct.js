3029655d3b04bc128978a614c39eed5784f07427
vlk2022 <volodymyrkostiuk2021@gmail.com>
2024-03-13 23:40:48 +0200

    commit

--- src/pages/SingleProduct/SingleProduct.js
+++ src/pages/SingleProduct/SingleProduct.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 import classes from "./SingleProduct.module.css";
+import {useSelector} from "react-redux";
 
 
 const SingleProduct = () => {
+    const {productsArr} = useSelector(store => store.products);
+
+
     return (
         <main className={`${classes.wrap} width`}>
             SingleProduct
