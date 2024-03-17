328578c322a3615c18fa35fe950b32bb7a42ba21
vlk2022 <volodymyrkostiuk2021@gmail.com>
2024-03-17 09:31:42 +0200

    created change color title in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link} from "react-router-dom";
 
 import classes from "./Header.module.css";
@@ -6,10 +6,27 @@ import {Cart} from "../Cart/Cart";
 
 
 const Header = () => {
+    const colors = ['#7E72F2', '#2C2C2C', 'yellow', 'green'];
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentIndex(prev => (prev + 1) % colors.length);
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+
+
+    const currentColor = colors[currentIndex];
+
+
     return (
         <main className={`${classes.wrap} width flex`}>
             <Link to={'/'}>
-                <h1 className={`${classes.blockTitle}`}>Online store</h1>
+                <h1 className={`${classes.blockTitle}`} style={{color: currentColor}}>Online store</h1>
             </Link>
 
             <section className={`${classes.blockCart}`}>
